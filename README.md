0e3d6b17d3335772615ece82d9b52868fc9340df
mattyschell <mattyschell@gmail.com>
2025-12-08 12:11:28 -0500

    resolves #1

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-# CSCL-QA
+# CSCL_QA
 
-Miscellaneous quality assurance checks on NYC's top banana geodatabase.  Friends, this is our quality assurance of the top banana, our rules, the trick is never to be afraid.
+Miscellaneous quality assurance checks of NYC's top banana geodatabase.  Friends, this is our banana quality assurance, our rules, the trick is never to be afraid.
+
+### QA a CSCL dataset
+
+Report suspect values.  Intended for use with a live CSCL version.
+
+```text
+usage: qa_one_dataset.py [-h] dataset geodatabase logdir badattribute badattributecolumn
+
+QA a CSCL dataset
+
+positional arguments:
+  dataset             Dataset name in cscl
+  geodatabase         Path to the cscl geodatabase version
+  logdir              Folder for logs
+  badattribute        Known junk value (ex junk)
+  badattributecolumn  Column to check for junk
+
+options:
+  -h, --help          show this help message and exit
+```
+
 
 ### QA a child replica dataset
+
+Report bad counts and suspect values in a parent-child dataset pair.
 
 ```text
 usage: qa_child_dataset.py [-h] [--badattribute BADATTRIBUTE] [--badattributecolumn BADATTRIBUTECOLUMN]
---
+++ py/qa_one_dataset.py
@@ -0,0 +1,70 @@
+import os
+import time
+import sys
+import logging
+import argparse
+import arcpy
+
+import cscl_dataset
+
+def setuplogger(loggername
+               ,datasetname
+               ,logdirectory):
+
+    # ..geodatabase-scripts\logs\qa-borough-DEFAULT-20250403-160745.log
+    targetlog = os.path.join(logdirectory 
+                            ,'qa-{0}-{1}.log'.format(datasetname
+                                                    ,time.strftime("%Y%m%d-%H%M%S")))
+
+    logger = logging.getLogger(loggername)
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(targetlog, mode='w')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+def main():
+
+    parser = argparse.ArgumentParser(description="QA a CSCL dataset")
+
+    # Required arguments
+    parser.add_argument("dataset", help="Dataset name in cscl")
+    parser.add_argument("geodatabase", help="Path to the cscl geodatabase version")
+    parser.add_argument("logdir", help="Folder for logs")
+    parser.add_argument("badattribute", help="Known junk value (ex junk)")
+    parser.add_argument("badattributecolumn", help="Column to check for junk")
+
+    args = parser.parse_args()
+
+    gdbversion = arcpy.Describe(args.geodatabase).connectionProperties.version.split('.')[-1]
+
+    # add version to log name since qa_child_dataset.py logs use only dataset
+    setuplogger('qa_dataset'
+               ,'{0}-{1}'.format(args.dataset, gdbversion)
+               ,args.logdir)
+    logger = logging.getLogger('qa_dataset')
+
+    logger.info('starting qa of {0} {1}'.format(args.dataset
+                                               ,args.badattributecolumn))
+
+    cscldataset = cscl_dataset.CSCLDataset(args.dataset)
+    badkount    = 0
+
+    if cscldataset.attribute_exists(args.geodatabase
+                                   ,args.badattributecolumn
+                                   ,args.badattribute):
+        badkount +=1
+        logger.warning('QA: bad {0} value {1} checking version {2}'.format(args.badattributecolumn
+                                                                            ,args.badattribute
+                                                                            ,gdbversion))  
+    else:
+        logger.info('PASS: no value {0} in {1} checking version {2}'.format(args.badattribute
+                                                                            ,args.badattributecolumn
+                                                                            ,gdbversion)) 
+
+    sys.exit(badkount)
+
+if __name__ == '__main__':
+    main()
