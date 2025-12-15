0e56b09ec07987eb8085f633348a358fb30062a8
mattyschell <mattyschell@gmail.com>
2025-12-15 10:25:09 -0500

    adds sample qa script for the real deal

---
+++ geodatabase-scripts/sample-qa-version.bat
@@ -0,0 +1,68 @@
+@echo off
+set ENV=xxx
+set BASEPATH=X:\xxx
+set DB=xxxxxxx
+set LOGDIR=%BASEPATH%\geodatabase-scripts\logs\cscl_qa
+set CSCLLAYER1=xxxx.xxxxxxxxx
+set QAVALUE1=""
+set QACOLUMN1=xxxxxxxx
+set CSCLLAYER2=xxxx.xxxxxxxxxx
+set QAVALUE2=JUNK
+set QACOLUMN2=xxxxxxxxx
+set QAVERSION=XXXXXXXVERSION
+set CSCLGDB=%BASEPATH%\Connections\oracle19c\%ENV%\CSCL-%DB%\xxxxxxxxxx_xxxversion.sde
+set NOTIFY=xxxxxx@xxx.xxx
+set NOTIFYFROM=xxxxx@xxx.xxx.xxx
+set SMTPFROM=xxxxxxxxxx.xxxxxx
+set PYTHON1=C:\Python27\ArcGIS10.7\python.exe
+set PYTHON2=C:\Python27\ArcGIS10.8\python.exe
+if exist "%PYTHON1%" (
+    set PROPY=%PYTHON1%
+) else if exist "%PYTHON2%" (
+    set PROPY=%PYTHON2%
+) 
+set BATLOG=%LOGDIR%\%ENV%-%QAVERSION%.log
+echo QAing %ENV% %CSCLLAYER1% on %date% at %time% > %BATLOG%
+set FAILCOUNT=0
+CALL %PROPY% %BASEPATH%\cscl_qa\py\qa_one_dataset.py ^
+             %CSCLLAYER1% ^
+             %CSCLGDB% ^
+             %LOGDIR% ^
+             %QAVALUE1% ^
+             %QACOLUMN1%  
+if %ERRORLEVEL% NEQ 0 (
+    set /a FAILCOUNT+=1
+    echo. >> %BATLOG%
+    echo QA fail sending notification >> %BATLOG%
+    CALL %PROPY% %BASEPATH%\cscl_qa\py\notify.py "QA Report for %CSCLLAYER1% (%ENV%)" %NOTIFY% qa-%CSCLLAYER1%-%QAVERSION% %LOGDIR% %NOTIFYFROM% %SMTPFROM%
+) 
+echo. >> %BATLOG% && echo completed %ENV% %CSCLLAYER1% on %date% at %time% >> %BATLOG%
+echo QAing %ENV% %CSCLLAYER2% on %date% at %time% > %BATLOG%
+CALL %PROPY% %BASEPATH%\cscl_qa\py\qa_one_dataset.py ^
+             %CSCLLAYER2% ^
+             %CSCLGDB% ^
+             %LOGDIR% ^
+             %QAVALUE2% ^
+             %QACOLUMN2%  
+if %ERRORLEVEL% NEQ 0 (
+    set /a FAILCOUNT+=1
+    echo. >> %BATLOG%
+    echo QA fail sending notification >> %BATLOG%
+    CALL %PROPY% %BASEPATH%\cscl_qa\py\notify.py "QA Report for %CSCLLAYER2% (%ENV%)" %NOTIFY% qa-%CSCLLAYER2%-%QAVERSION% %LOGDIR% %NOTIFYFROM% %SMTPFROM%
+) 
+echo. >> %BATLOG% && echo completed %ENV% %CSCLLAYER2% on %date% at %time% >> %BATLOG%
+if %FAILCOUNT% NEQ 0 (
+    echo    .------.    .------.    .------.
+    echo   /  STOP  \  /  STOP  \  /  STOP  \
+    echo   \   QA   /  \   QA   /  \   QA   /
+    echo    '------'    '------'    '------' 
+)
+if %FAILCOUNT% NEQ 0 (
+    echo Check your email for QA details
+    echo Continue without reconciling and posting %QAVERSION%
+    echo Acknowledge you have seen this by
+    pause
+)
+exit /b 0
+
+   
